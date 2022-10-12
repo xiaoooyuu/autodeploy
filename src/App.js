597f6c16375789e6db82a0e69351ca1bbf0967e7
xy <xythesmallfish@gmail.com>
2022-10-12 16:48:40 +0800

    testing 123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import React from 'react';
 import './App.css';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> HELLO WORLD</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={HomePage} />
+
+        <Route path="/dashboard" element={DashboardPage} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+    </div>
+  )
+}
+     
 export default App;
